6d4f1bd46d5750f569b7a632697d3057f8e2f8b6
Roberto Sousa <robertosousa186@gmail.com>
2021-05-04 22:42:51 -0300

    separating code by responsibility

--- src/server.js
+++ src/server.js
@@ -1,16 +1,6 @@
-import express from "express";
-import bodyParser from "body-parser";
+import app from "./app";
 
 const PORT = 3000;
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send({
-    firstServer: "Only for test",
-    Status: "OK!",
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
