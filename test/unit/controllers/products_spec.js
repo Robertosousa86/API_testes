fff9a8d622ef7cb14137d0e8c3e02f3c8ab72ff4
Roberto Sousa <robertosousa186@gmail.com>
2021-05-24 23:22:51 -0300

    creating the initial test structure of the get method

---
+++ test/unit/controllers/products_spec.js
@@ -0,0 +1,8 @@
+// Engloba todos os testes do controller de products.
+describe("Controllers: Products", () => {
+  describe("get() products", () => {
+    it("Should return a list of products", () => {
+
+    });
+  });
+});
