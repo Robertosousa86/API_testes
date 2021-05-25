10754a62624f9de6f43b0bbf25cab3cd540c17a2
Roberto Sousa <robertosousa186@gmail.com>
2021-05-25 20:50:41 -0300

    creating an instance of productsController

--- test/unit/controllers/products_spec.js
+++ test/unit/controllers/products_spec.js
@@ -1,8 +1,13 @@
 // Engloba todos os testes do controller de products.
+import ProductsController from "../../../src/controllers/products";
+
 describe("Controllers: Products", () => {
   describe("get() products", () => {
     it("Should return a list of products", () => {
 
+
+      const productsController = new ProductsController();
+      
     });
   });
 });
