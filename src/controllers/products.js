350349618b1405e17b63051cebd06c036bb2209c
Roberto Sousa <robertosousa186@gmail.com>
2021-05-25 23:24:11 -0300

    adding function get: GREEN

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,5 +1,14 @@
+// O método get deve receber os objetos de requisição e resposta e enviar um array com um produto srestático como resposta.
 class ProductsController {
-
+  get(req, res) {
+    return res.send([
+      {
+        name: "Default product",
+        description: "product description",
+        price: 100,
+      },
+    ]);
+  }
 }
 
-export default ProductsController
+export default ProductsController;
