0a4b0b272b307e98ae08af1f6d9b763cba954812
Roberto Sousa <robertosousa186@gmail.com>
2021-05-28 03:09:04 -0300

    updating app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
-// Tem a responsábilidade de iniciar e carregar os middlewares
+// Tem a responsábilidade de iniciar a aplicação e carregar os middlewares
 import express from 'express';
 import routes from './routes';
+import database from './config/database';
 
 const app = express();
+// Configura o express e retornar uma nova instância da aplicação configurada.
+const configureExpress = () => {
+  app.use('/', routes);
+  app.database = database;
 
-app.use('/', routes);
+  return app;
+};
+// Exporta uma função que retorna uma promise, assim que a promise for resolvida significa que o DB está diponível.
+export default async () => {
+  const app = configureExpress();
+  await app.database.connect();
 
-export default app;
+  return app;
+};
