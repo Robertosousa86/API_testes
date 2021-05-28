13438bde3be0d8c36927cb0f1656d4135456f2d9
Roberto Sousa <robertosousa186@gmail.com>
2021-05-28 00:50:23 -0300

    creating code pattern with eslint and prettier

--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
 // O server.js é o arquivo responsável por inicializar a aplicação, chamando o app.
-import app from "./app";
-require('dotenv').config()
+import app from './app';
+require('dotenv').config();
 
 const PORT = process.env.HOST;
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
-
-const name = process.env.MONGODB_URL
-
-console.log(name);
