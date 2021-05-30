c5f620a9f05a831dd0102fbd4a0dd51cd7e28de1
Roberto Sousa <robertosousa186@gmail.com>
2021-05-30 03:10:49 -0300

    update proucts_spec

--- test/integration/routes/products_spec.js
+++ test/integration/routes/products_spec.js
@@ -1,4 +1,16 @@
 describe('Routes: Products', () => {
+  let request;
+  let app;
+
+  before(async () => {
+    // eslint-disable-next-line no-undef
+    app = await setupApp();
+    // eslint-disable-next-line no-undef
+    request = supertest(app);
+  });
+
+  after(async () => await app.database.connection.close());
+
   const defaultProduct = {
     name: 'Default product',
     description: 'product description',
@@ -8,6 +20,7 @@ describe('Routes: Products', () => {
   describe('GET /products', () => {
     it('should return a list of products', (done) => {
       request.get('/products').end((err, res) => {
+        // eslint-disable-next-line no-undef
         expect(res.body[0]).to.eql(defaultProduct);
         done(err);
       });
@@ -22,6 +35,7 @@ describe('Routes: Products', () => {
   describe('GET /', () => {
     it('should return default response', (done) => {
       request.get('/').end((err, res) => {
+        // eslint-disable-next-line no-undef
         expect(res.body).to.eql(defaultResponse);
         done(err);
       });
