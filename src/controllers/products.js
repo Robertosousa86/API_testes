b2dc886adef593fa62f1502992c45525367c6113
Roberto Sousa <robertosousa186@gmail.com>
2021-06-13 00:29:51 -0300

    updating the controller to use the model

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,13 +1,12 @@
 // O método get deve receber os objetos de requisição e resposta e enviar um array com um produto srestático como resposta.
 class ProductsController {
-  get(req, res) {
-    return res.send([
-      {
-        name: 'Default product',
-        description: 'product description',
-        price: 100,
-      },
-    ]);
+  constructor(Product) {
+    this.Product = Product;
+  }
+
+  async get(req, res) {
+    const products = await this.Product.find({});
+    res.send(products);
   }
 }
 
