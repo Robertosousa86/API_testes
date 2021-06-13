1cd006a37d019122f908d5e8a1d252c78430d9ce
Roberto Sousa <robertosousa186@gmail.com>
2021-06-13 03:59:27 -0300

    testing error cases

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,12 +1,16 @@
-// O método get deve receber os objetos de requisição e resposta e enviar um array com um produto srestático como resposta.
+// O método get deve receber os objetos de requisição e resposta e enviar um array com um produto estático como resposta.
 class ProductsController {
   constructor(Product) {
     this.Product = Product;
   }
 
   async get(req, res) {
-    const products = await this.Product.find({});
-    res.send(products);
+    try {
+      const products = await this.Product.find({});
+      res.send(products);
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
   }
 }
 
